79648cd88f5662c4cb749f8efb90505baba43208
Juho Inkinen <juho.inkinen@helsinki.fi>
2023-04-28 15:31:42 +0300

    wip Try upgrading to Connexion 3

--- annif/__init__.py
+++ annif/__init__.py
@@ -5,9 +5,11 @@ import os
 import os.path
 
 import connexion
+
+# from flask import Flask
 from flask_cors import CORS
 
-from annif.openapi.validation import CustomRequestBodyValidator
+# from annif.openapi.validation import CustomRequestBodyValidator
 
 logging.basicConfig()
 logger = logging.getLogger("annif")
@@ -21,7 +23,8 @@ def create_app(config_name=None):
     # as a property (cxapp.app)
 
     specdir = os.path.join(os.path.dirname(__file__), "openapi")
-    cxapp = connexion.App(__name__, specification_dir=specdir)
+    # cxapp = connexion.App(__name__, specification_dir=specdir)
+    cxapp = connexion.FlaskApp(__name__, specification_dir=specdir)
     if config_name is None:
         config_name = os.environ.get("ANNIF_CONFIG")
     if config_name is None:
@@ -33,10 +36,10 @@ def create_app(config_name=None):
     cxapp.app.config.from_object(config_name)
     cxapp.app.config.from_envvar("ANNIF_SETTINGS", silent=True)
 
-    validator_map = {
-        "body": CustomRequestBodyValidator,
-    }
-    cxapp.add_api("annif.yaml", validator_map=validator_map)
+    # validator_map = {
+    #     "body": CustomRequestBodyValidator,
+    # }
+    cxapp.add_api("annif.yaml")  # validator_map=validator_map)
 
     # add CORS support
     CORS(cxapp.app)
@@ -52,3 +55,45 @@ def create_app(config_name=None):
 
     # return the Flask app
     return cxapp.app
+
+
+# def create_app(config_name=None):
+#     # 'cxapp' here is the Connexion application that has a normal Flask app
+#     # as a property (cxapp.app)
+
+#     specdir = os.path.join(os.path.dirname(__file__), "openapi")
+#     app = Flask(__name__)
+
+#     if config_name is None:
+#         config_name = os.environ.get("ANNIF_CONFIG")
+#     if config_name is None:
+#         if os.environ.get("FLASK_RUN_FROM_CLI") == "true":
+#             config_name = "annif.default_config.Config"
+#         else:
+#             config_name = "annif.default_config.ProductionConfig"
+#     logger.debug("creating app with configuration %s", config_name)
+#     app.config.from_object(config_name)
+#     app.config.from_envvar("ANNIF_SETTINGS", silent=True)
+
+#     cxapp = connexion.ConnexionMiddleware(
+#         app,
+#         specification_dir=specdir,
+#     )
+#     cxapp.add_api(
+#         "annif.yaml"
+#     )
+
+#     if cxapp.app.config["INITIALIZE_PROJECTS"]:
+#         annif.registry.initialize_projects(cxapp.app)
+#         logger.info("finished initializing projects")
+
+#     # add CORS support
+#     CORS(cxapp.app)
+
+#     # register the views via blueprints
+#     from annif.views import bp
+
+#     cxapp.app.register_blueprint(bp)
+
+#     # return the Flask app
+#     return cxapp.app
